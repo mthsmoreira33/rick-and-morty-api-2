d138dc1044ff4963f1dca20f1faac56f4d18f1f8
Matheus Moreira <mthsmoreira59@gmail.com>
2023-10-06 09:41:17 -0300

    consumo inicial da api rick and morty

---
+++ js/index.js
@@ -0,0 +1,10 @@
+const api = axios.create({
+  baseURL: "https://rickandmortyapi.com/api",
+});
+
+const endpoint = "/character/";
+let currentPage = 1;
+const PER_PAGE = 6;
+let response;
+
+let isLoading
